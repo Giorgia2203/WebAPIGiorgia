97f462328dd3ccc93c7d4e483fdba6f122dcc2ef
giorgia220523 <cocisgiorgia@gmail.com>
2022-12-20 14:41:51 +0200

    Add project files.

---
+++ Data/WebAPIGiorgiaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIGiorgia.Models;
+
+namespace WebAPIGiorgia.Data
+{
+    public class WebAPIGiorgiaContext : DbContext
+    {
+        public WebAPIGiorgiaContext (DbContextOptions<WebAPIGiorgiaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPIGiorgia.Models.ShopList> ShopList { get; set; } = default!;
+    }
+}
